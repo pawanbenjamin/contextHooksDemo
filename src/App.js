1df2c51dd7806b433ad3076149857e76fa7f2da8
pawanbenjamin <62716484+pawanbenjamin@users.noreply.github.com>
2022-04-11 11:10:48 -0400

    done

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import { Routes, Route, Link } from 'react-router-dom'
 import Home from './components/Home'
 import Me from './components/Me'
 
+import useAuth from './hooks/useAuth'
+
 function App() {
+  const { user } = useAuth()
+
+  console.log(user)
   return (
     <div>
       <header>
-        <h1>Hello There!</h1>
+        <h1>Welcome {user.name}!</h1>
         <Link to="/">Home</Link>
         <Link to="/me">Me</Link>
       </header>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,4 @@
-import useAuth from '../hooks/useAuth'
 const Home = () => {
-  const { user } = useAuth()
-  console.log({ user })
   return <div>The Home Page</div>
 }
 
