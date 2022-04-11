457c22b49100d2303f852889528e30c703dfeec8
pawanbenjamin <62716484+pawanbenjamin@users.noreply.github.com>
2022-04-10 22:18:58 -0400

    starting point

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import { Routes, Route, Link } from 'react-router-dom'
+import Home from './components/Home'
+import Me from './components/Me'
 
 function App() {
-  return <div style={{ textAlign: 'center' }}>Welcome to Fitness Tracker</div>
+  return (
+    <div>
+      <header>
+        <h1>Hello There!</h1>
+        <Link to="/">Home</Link>
+        <Link to="/me">Me</Link>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/me" element={<Me />} />
+      </Routes>
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/Me.jsx
@@ -0,0 +1,5 @@
+const Me = () => {
+  return <div>About Me!</div>
+}
+
+export default Me
